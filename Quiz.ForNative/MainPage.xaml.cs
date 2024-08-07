b34d9d5c2b09484ccca82c3f746d43155f5fc0bc
Cyril Auquier <cyrilauquier@hotmail.fr>
2024-08-07 16:05:39 +0200

    update main page to let use choose between login and register

--- Quiz.ForNative/MainPage.xaml.cs
+++ Quiz.ForNative/MainPage.xaml.cs
@@ -1,24 +1,21 @@
-﻿namespace Quiz.ForNative
+﻿using Quiz.ForNative.Views.Auth;
+
+namespace Quiz.ForNative
 {
     public partial class MainPage : ContentPage
     {
-        int count = 0;
-
         public MainPage()
         {
             InitializeComponent();
         }
 
-        private void OnCounterClicked(object sender, EventArgs e)
+        private void OnLogin(object sender, EventArgs e)
         {
-            count++;
+        }
 
-            if (count == 1)
-                CounterBtn.Text = $"Clicked {count} time";
-            else
-                CounterBtn.Text = $"Clicked {count} times";
-
-            SemanticScreenReader.Announce(CounterBtn.Text);
+        private async void OnRegister(object sender, EventArgs e)
+        {
+            await Shell.Current.GoToAsync(nameof(RegisterView));
         }
     }
 
