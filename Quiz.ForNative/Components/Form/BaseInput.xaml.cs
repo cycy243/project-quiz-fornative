0e5b2fe8edabdff1618f14b85176b18f8c39efc5
Cyril Auquier <cyrilauquier@hotmail.fr>
2024-08-08 15:44:20 +0200

    when label clicked then input has focus

--- Quiz.ForNative/Components/Form/BaseInput.xaml.cs
+++ Quiz.ForNative/Components/Form/BaseInput.xaml.cs
@@ -1,3 +1,5 @@
+using System.Windows.Input;
+
 namespace Quiz.ForNative.Components.Form;
 
 public partial class BaseInput : ContentView
@@ -7,6 +9,19 @@ public partial class BaseInput : ContentView
         typeof(string),
         typeof(BaseInput),
         default(string));
+    public static readonly BindableProperty LabelClickedCommandProperty = BindableProperty.Create(
+        nameof(LabelClickedCommandProperty),
+        typeof(string),
+        typeof(BaseInput),
+        default(string));
+
+    ContentPresenter InputEntry;
+    Label InputLabel;
+
+    public ICommand LabelClickedCommand => new Command(() =>
+    {
+
+    });
 
     public string LabelText
     {
@@ -17,6 +32,43 @@ public partial class BaseInput : ContentView
     public BaseInput()
     {
         InitializeComponent();
-        this.BindingContext = this;
+        //LabelClickedCommand = new Command(() =>
+        //{
+        //    (this.baseInput.FindByName("input_container") as Frame).Focus();
+        //});
     }
+
+    // Event handler for when the Label is tapped
+    private void OnLabelTapped(object sender, EventArgs e)
+    {
+        // Get the first child in the ContentPresenter that is focusable
+        if (FindVisualElement<ContentPresenter>(this).Content is View view)
+        {
+            view.Focus(); // Set focus to the child view
+        }
+    }
+
+    // Utility method to find an element of a specific type within the visual tree
+    private T FindVisualElement<T>(Element element) where T : Element
+    {
+        if (element is T typedElement)
+        {
+            return typedElement;
+        }
+
+        if (element is IVisualTreeElement visualTreeElement)
+        {
+            foreach (var child in visualTreeElement.GetVisualChildren())
+            {
+                var result = FindVisualElement<T>(child as Element);
+                if (result != null)
+                {
+                    return result;
+                }
+            }
+        }
+
+        return null;
+    }
+
 }
