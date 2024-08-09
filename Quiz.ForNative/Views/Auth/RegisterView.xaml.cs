09edd4a70e50aee7f75261eb20274d661cc06222
Cyril Auquier <cyrilauquier@hotmail.fr>
2024-08-09 14:30:23 +0200

    add validation error for email input

--- Quiz.ForNative/Views/Auth/RegisterView.xaml.cs
+++ Quiz.ForNative/Views/Auth/RegisterView.xaml.cs
@@ -1,9 +1,30 @@
+using Plugin.ValidationRules.Extensions;
+using Plugin.ValidationRules.Rules;
+
 namespace Quiz.ForNative.Views.Auth;
 
 public partial class RegisterView : ContentPage
 {
-	public RegisterView()
+
+    public RegisterView()
 	{
 		InitializeComponent();
-	}
+
+		InitializedValidationHandlers();
+    }
+
+	private void InitializedValidationHandlers()
+    {
+
+        var EmailValidator = Validator.Build<string>()
+            .WithRule(new EmailRule(), "Email is not valide");
+
+
+        MailInput.ValidationFunction += (inputName, value) =>
+        {
+            EmailValidator.Value = value;
+            var errors = EmailValidator.Validate();
+            return EmailValidator.Error;
+        };
+    }
 }
