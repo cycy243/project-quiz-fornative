4f4c2ca252227a260f8988aa1a00592fe5394d6a
Cyril Auquier <cyrilauquier@hotmail.fr>
2024-08-18 15:25:46 +0200

    update rules

---
+++ Quiz.Validations/MaximumLengthRule.cs
@@ -0,0 +1,26 @@
+﻿using Plugin.ValidationRules.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Quiz.Validations
+{
+    public class MaximumLengthRule<T> : IValidationRule<T>
+    {
+        public string ValidationMessage { get; set; }
+
+        private int _maximumLength { get; set; }
+
+        public MaximumLengthRule(int maximumLength)
+        {
+            this._maximumLength = maximumLength;
+        }
+
+        public bool Check(T value)
+        {
+            return value?.ToString()?.Length <= _maximumLength;
+        }
+    }
+}
---
+++ Quiz.Validations/UserIsAtLeast13YearsOldRule.cs
@@ -0,0 +1,25 @@
+﻿using Plugin.ValidationRules.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace Quiz.Validations
+{
+    public class UserIsAtLeast13YearsOldRule : IValidationRule<DateOnly>
+    {
+        public string ValidationMessage { get; set; }
+
+        public bool Check(DateOnly value)
+        {
+            var now = DateTime.Now;
+            if(value.Year <= (now.Year - 13))
+            {
+                return value.Month <= now.Month && value.Day <= now.Day;
+            }
+            return false;
+        }
+    }
+}
