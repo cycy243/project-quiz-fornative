403c7c03980619dc707e36d42f7ce65580740ce3
Cyril Auquier <cyrilauquier@hotmail.fr>
2024-08-18 15:26:20 +0200

    add input for long text entry

---
+++ Quiz.ForNative/Components/Form/LongTextInput.xaml.cs
@@ -0,0 +1,61 @@
+namespace Quiz.ForNative.Components.Form;
+
+public partial class LongTextInput : ContentView, IFormInput<string>
+{
+    public static readonly BindableProperty LabelProperty = BindableProperty.Create(
+        nameof(LabelContent),
+        typeof(string),
+        typeof(LongTextInput),
+        default(string));
+    public static readonly BindableProperty PlaceholderProperty = BindableProperty.Create(
+        nameof(PlaceholderContent),
+        typeof(string),
+        typeof(LongTextInput),
+        default(string));
+    public static readonly BindableProperty ValidationRulesProperty = BindableProperty.Create(
+        nameof(ValidationRules),
+        typeof(string),
+        typeof(LongTextInput),
+        default(string));
+    public static readonly BindableProperty ValidationFunctionProperty = BindableProperty.Create(
+        nameof(ValidationFunction),
+        typeof(InputValidationFunction),
+        typeof(LongTextInput));
+    public static readonly BindableProperty InputNameProperty = BindableProperty.Create(
+        nameof(InputName),
+        typeof(string),
+        typeof(LongTextInput),
+        default(string));
+
+    public InputValidationFunction ValidationFunction
+    {
+        get => GetValue(ValidationFunctionProperty) as InputValidationFunction;
+        set => SetValue(ValidationFunctionProperty, value);
+    }
+
+    public string LabelContent
+    {
+        get => GetValue(LabelProperty) as string;
+        set => SetValue(LabelProperty, value);
+    }
+    public string PlaceholderContent
+    {
+        get => GetValue(PlaceholderProperty) as string;
+        set => SetValue(PlaceholderProperty, value);
+    }
+    public string ValidationRules
+    {
+        get => GetValue(ValidationRulesProperty) as string;
+        set => SetValue(ValidationRulesProperty, value);
+    }
+
+    public string InputName
+    {
+        get => GetValue(InputNameProperty) as string;
+        set => SetValue(InputNameProperty, value);
+    }
+    public LongTextInput()
+	{
+		InitializeComponent();
+	}
+}
