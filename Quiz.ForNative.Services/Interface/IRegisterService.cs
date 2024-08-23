95c00412e3ecf72a79ab5e0f1397130bdcded127
Cyril Auquier <cyrilauquier@hotmail.fr>
2024-08-23 21:03:47 +0200

    create register service interface

---
+++ Quiz.ForNative.Services/Interface/IRegisterService.cs
@@ -0,0 +1,25 @@
+﻿using Quiz.Dtos.Api;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Quiz.ForNative.Services.Interface
+{
+    /// <summary>
+    /// Service that help us to register user
+    /// </summary>
+    /// <typeparam name="T">Type that represents a user</typeparam>
+    public interface RegisterService<T>
+    {
+        /// <summary>
+        /// Register a user
+        /// </summary>
+        /// <param name="entity">User to register</param>
+        /// <returns>
+        /// Return the added user
+        /// </returns>
+        Task<T> RegisterUser(T entity);
+    }
+}
