3adcb006967f98394e3a04b0715c061300f5d5af
Cyril Auquier <cyrilauquier@hotmail.fr>
2024-08-23 21:02:12 +0200

    test http handler behaviors

---
+++ Quiz.ForNative.Repository.Tests/ConflictHandlerTests.cs
@@ -0,0 +1,61 @@
+﻿using FluentAssertions;
+using Microsoft.AspNetCore.Http;
+using Moq;
+using Moq.Protected;
+using Quiz.ForNative.Repository.Exceptions;
+using Quiz.ForNative.Tests.Utils.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Quiz.ForNative.Middleware.Http.Tests
+{
+    [TestClass]
+    public class ConflictHandlerTests
+    {
+        [TestMethod]
+        public async Task WhenHttpClientGetResponseWithConflictStatusThenThrowError()
+        {
+            // Arrange
+            var handlerMock = HttpMessageHandlerMock.CreateHttpMessageHandler(HttpStatusCode.Conflict, new StringContent("Conflict response content"));
+
+            var unAuthorizedAccessHandler = new ConflictHandler
+            {
+
+                InnerHandler = handlerMock.Object
+            };
+
+            var httpClient = new HttpClient(unAuthorizedAccessHandler);
+
+            // Act
+            Func<Task> action = async () => await httpClient.GetAsync("https://www.google.com");
+
+            // Assert
+            await action.Should().ThrowAsync<RessourceAlreadyExistsException>();
+        }
+
+        [TestMethod]
+        public async Task WhenHttpClientGetResponseWithOkStatusThenDoesNotThrowError()
+        {
+            // Arrange
+            var handlerMock = HttpMessageHandlerMock.CreateHttpMessageHandler(HttpStatusCode.OK, new StringContent("ok response content"));
+
+            var unAuthorizedAccessHandler = new ConflictHandler
+            {
+
+                InnerHandler = handlerMock.Object
+            };
+
+            var httpClient = new HttpClient(unAuthorizedAccessHandler);
+
+            // Act
+            Func<Task> action = async () => await httpClient.GetAsync("https://www.google.com");
+
+            // Assert
+            await action.Should().NotThrowAsync<UnauthorizedAccessException>();
+        }
+    }
+}
---
+++ Quiz.ForNative.Repository.Tests/UnAuthorizedAccessHandlerTests.cs
@@ -0,0 +1,74 @@
+﻿using FluentAssertions;
+using Quiz.ForNative.Tests.Utils.Http;
+using System.Net;
+using Exceptions = Quiz.ForNative.Repository.Exceptions;
+
+namespace Quiz.ForNative.Middleware.Http.Tests
+{
+    [TestClass]
+    public class UnAuthorizedAccessHandlerTests
+    {
+        [TestMethod]
+        public async Task WhenHttpClientGetResponseWithUnAuthorizedStatusThenThrowError()
+        {
+            // Arrange
+            var handlerMock = HttpMessageHandlerMock.CreateHttpMessageHandler(HttpStatusCode.Unauthorized, new StringContent("Unauthorized response content"));
+
+            var unAuthorizedAccessHandler = new UnAuthorizedAccessHandler
+            {
+
+                InnerHandler = handlerMock.Object
+            };
+
+            var httpClient = new HttpClient(unAuthorizedAccessHandler);
+
+            // Act
+            Func<Task> action = async () => await httpClient.GetAsync("https://www.google.com");
+
+            // Assert
+            await action.Should().ThrowAsync<Exceptions.UnAuthorizedAccessException>();
+        }
+
+        [TestMethod]
+        public async Task WhenHttpClientGetResponseWithForbiddenStatusThenThrowError()
+        {
+            // Arrange
+            var handlerMock = HttpMessageHandlerMock.CreateHttpMessageHandler(HttpStatusCode.Forbidden, new StringContent("Forbidden response content"));
+
+            var unAuthorizedAccessHandler = new UnAuthorizedAccessHandler
+            {
+
+                InnerHandler = handlerMock.Object
+            };
+
+            var httpClient = new HttpClient(unAuthorizedAccessHandler);
+
+            // Act
+            Func<Task> action = async () => await httpClient.GetAsync("https://www.google.com");
+
+            // Assert
+            await action.Should().ThrowAsync<Exceptions.UnAuthorizedAccessException>();
+        }
+
+        [TestMethod]
+        public async Task WhenHttpClientGetResponseWithOkStatusThenDoesNotThrowError()
+        {
+            // Arrange
+            var handlerMock = HttpMessageHandlerMock.CreateHttpMessageHandler(HttpStatusCode.OK, new StringContent("ok response content"));
+
+            var unAuthorizedAccessHandler = new UnAuthorizedAccessHandler
+            {
+
+                InnerHandler = handlerMock.Object
+            };
+
+            var httpClient = new HttpClient(unAuthorizedAccessHandler);
+
+            // Act
+            Func<Task> action = async () => await httpClient.GetAsync("https://www.google.com");
+
+            // Assert
+            await action.Should().NotThrowAsync<UnauthorizedAccessException>();
+        }
+    }
+}
