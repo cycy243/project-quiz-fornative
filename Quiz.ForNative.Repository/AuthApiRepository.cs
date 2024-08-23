d67620129637cb35205403a63b55cc96ae663d89
Cyril Auquier <cyrilauquier@hotmail.fr>
2024-08-23 21:02:56 +0200

    implement user auth interface

---
+++ Quiz.ForNative.Repository/AuthApiRepository.cs
@@ -0,0 +1,29 @@
+﻿using Newtonsoft.Json;
+using Quiz.Dtos.App;
+using Quiz.ForNative.Repository.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Json;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Quiz.ForNative.Repository
+{
+    public class AuthApiRepository : IAuthRepository<UserDto>
+    {
+        private HttpClient _httpClient { get; init; }
+
+        public AuthApiRepository(HttpClient httpClient)
+        {
+            _httpClient = httpClient;
+        }
+
+        public async Task<UserDto> RegisterUser(UserDto user)
+        {
+            HttpResponseMessage response = (await _httpClient.PostAsJsonAsync(new Uri("/auth/register"), user));
+            return JsonConvert.DeserializeObject<UserDto>(await response.Content.ReadAsStringAsync())!;
+        }
+    }
+}
