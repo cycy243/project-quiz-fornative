e59cd2a4ba7095a8ecf94701fdab3d920851654f
Cyril Auquier <cyrilauquier@hotmail.fr>
2024-08-23 21:04:31 +0200

    add utils to create httpclienthandler for each type of need

---
+++ Quiz.ForNative.Tests.Utils/Http/HttpMessageHandlerMock.cs
@@ -0,0 +1,28 @@
+﻿using Moq;
+using Moq.Protected;
+using System.Net;
+
+namespace Quiz.ForNative.Tests.Utils.Http
+{
+    public static class HttpMessageHandlerMock
+    {
+        public static Mock<HttpMessageHandler> CreateHttpMessageHandler(HttpStatusCode statusCode, HttpContent messageContent)
+        {
+            var handlerMock = new Mock<HttpMessageHandler>();
+
+            handlerMock
+                .Protected()
+                .Setup<Task<HttpResponseMessage>>(
+                    "SendAsync",
+                    ItExpr.IsAny<HttpRequestMessage>(),
+                    ItExpr.IsAny<CancellationToken>()
+                )
+                .ReturnsAsync(new HttpResponseMessage
+                {
+                    StatusCode = statusCode,
+                    Content = messageContent
+                });
+            return handlerMock;
+        }
+    }
+}
