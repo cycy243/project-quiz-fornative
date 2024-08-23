7497afd0bf8908e1fdcc973f5c146e7972ba2eca
Cyril Auquier <cyrilauquier@hotmail.fr>
2024-08-23 21:04:00 +0200

    implements register service interface

---
+++ Quiz.ForNative.Services/RegisterService.cs
@@ -0,0 +1,32 @@
+﻿using Quiz.Dtos.Api;
+using Quiz.Dtos.App;
+using Quiz.ForNative.Args;
+using Quiz.ForNative.Repository.Interfaces;
+using Quiz.ForNative.Services.Interface;
+
+namespace Quiz.ForNative.Services
+{
+    public class RegisterService : RegisterService<RegisterDto>
+    {
+        private IRepository<UserDto, UserSearchArgs> _userRepository;
+
+        public RegisterService(IRepository<UserDto, UserSearchArgs> userRepository)
+        {
+            _userRepository = userRepository;
+        }
+
+        public async Task<RegisterDto> RegisterUser(RegisterDto entity)
+        {
+            bool registerResult = await _userRepository.Add(new UserDto(
+                Bio: entity.Bio,
+                Email: entity.Email,
+                Firstname: entity.Firstname,
+                Name: entity.Name,
+                Password: entity.Password,
+                PicturePath: entity.PicturePath,
+                Pseudo: entity.Pseudo
+            ));
+            return registerResult ? entity : throw new Exception("An error occured while saving the user");
+        }
+    }
+}
