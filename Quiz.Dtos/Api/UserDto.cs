07585bb97db4defc5956fcc647c89d18b419cc1b
Cyril Auquier <cyrilauquier@hotmail.fr>
2024-08-23 21:54:56 +0200

    add api dto for user data

---
+++ Quiz.Dtos/Api/UserDto.cs
@@ -0,0 +1,4 @@
+﻿namespace Quiz.Dtos.Api
+{
+    public record UserDto(string Name, string Firstname, string Pseudo, string Email, string PicturePath, string Bio, string? Id = null, string? Password = null, string? token = null);
+}
