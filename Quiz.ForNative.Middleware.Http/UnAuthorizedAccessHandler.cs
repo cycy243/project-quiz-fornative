e8276835247dd03466231e13f1f78c34741bb697
Cyril Auquier <cyrilauquier@hotmail.fr>
2024-08-23 21:00:29 +0200

    add exceptions exposed by solution project

---
+++ Quiz.ForNative.Middleware.Http/UnAuthorizedAccessHandler.cs
@@ -0,0 +1,20 @@
+﻿using System.Net;
+using Exception = Quiz.ForNative.Repository.Exceptions;
+
+namespace Quiz.ForNative.Middleware.Http
+{
+    public class UnAuthorizedAccessHandler: DelegatingHandler
+    {
+        protected override async Task<HttpResponseMessage> SendAsync(HttpRequestMessage request, CancellationToken cancellationToken)
+        {
+            var response = await base.SendAsync(request, cancellationToken);
+
+            if (response != null && (response.StatusCode == HttpStatusCode.Unauthorized || response.StatusCode == HttpStatusCode.Forbidden))
+            {
+                throw new Exception.UnAuthorizedAccessException("User hasn't the right to consult the ressource.");
+            }
+
+            return response;
+        }
+    }
+}
