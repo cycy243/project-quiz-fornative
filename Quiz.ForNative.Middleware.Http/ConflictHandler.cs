9cd893a988e4eeef566fee1173d4a993e9ee6eee
Cyril Auquier <cyrilauquier@hotmail.fr>
2024-08-23 21:03:21 +0200

    add http handler to react to conflict response in http request

---
+++ Quiz.ForNative.Middleware.Http/ConflictHandler.cs
@@ -0,0 +1,20 @@
+﻿using Quiz.ForNative.Repository.Exceptions;
+using System.Net;
+
+namespace Quiz.ForNative.Middleware.Http
+{
+    public class ConflictHandler : DelegatingHandler
+    {
+        protected override async Task<HttpResponseMessage> SendAsync(HttpRequestMessage request, CancellationToken cancellationToken)
+        {
+            var response = await base.SendAsync(request, cancellationToken);
+
+            if (response != null && (response.StatusCode == HttpStatusCode.Conflict))
+            {
+                throw new RessourceAlreadyExistsException("User hasn't the right to consult the ressource.");
+            }
+
+            return response;
+        }
+    }
+}
