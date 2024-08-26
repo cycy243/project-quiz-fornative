c9065b8fa35079bf7cbe0986206af6dd022e6035
Cyril Auquier <cyrilauquier@hotmail.fr>
2024-08-26 12:16:12 +0200

    refactor so a mapper is used instead of writing conversion code here

--- Quiz.ForNative.Services/RegisterService.cs
+++ Quiz.ForNative.Services/RegisterService.cs
@@ -1,32 +1,34 @@
-﻿using ApiDtos = Quiz.Dtos.Api;
-using AppDtos = Quiz.Dtos.App;
-using Quiz.ForNative.Args;
+﻿using Quiz.ForNative.Args;
 using Quiz.ForNative.Repository.Interfaces;
 using Quiz.ForNative.Services.Interface;
+using Quiz.Dtos;
+using AutoMapper;
+using System.Net.Http.Headers;
 
 namespace Quiz.ForNative.Services
 {
-    public class RegisterService : RegisterService<ApiDtos.RegisterDto>
+    public class RegisterService : RegisterService<RegisterDto>
     {
-        private IRepository<ApiDtos.UserDto, UserSearchArgs> _userRepository;
+        private IAuthRepository<UserDto> _authRepository;
+        private IMapper _mapper;
 
-        public RegisterService(IRepository<ApiDtos.UserDto, UserSearchArgs> userRepository)
+        public RegisterService(IAuthRepository<UserDto> authRepository, IMapper mapper)
         {
-            _userRepository = userRepository;
+            _authRepository = authRepository;
+            _mapper = mapper;
         }
 
-        public async Task<ApiDtos.RegisterDto> RegisterUser(ApiDtos.RegisterDto entity)
+        public async Task<RegisterDto> RegisterUser(RegisterDto entity)
         {
-            bool registerResult = await _userRepository.Add(new ApiDtos.UserDto(
-                Bio: entity.Bio,
-                Email: entity.Email,
-                Firstname: entity.Firstname,
-                Name: entity.Name,
-                Password: entity.Password,
-                PicturePath: entity.PicturePath,
-                Pseudo: entity.Pseudo
-            ));
-            return registerResult ? entity : throw new Exception("An error occured while saving the user");
+            try
+            {
+                UserDto registerResult = await _authRepository.RegisterUser(_mapper.Map<UserDto>(entity), File.OpenRead(entity.PicturePath));
+                return entity ?? throw new Exception("An error occured while saving the user");
+            }
+            catch (Exception ex)
+            {
+                throw;
+            }
         }
     }
 }
