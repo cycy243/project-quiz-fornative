941209bac71639ef9bd93008816891bb01ca69e3
Cyril Auquier <cyrilauquier@hotmail.fr>
2024-08-26 12:10:42 +0200

    - rename and refactor classes

--- Quiz.Dtos/Api/UserDto.cs
+++
@@ -1,4 +0,0 @@
-﻿namespace Quiz.Dtos.Api
-{
-    public record UserDto(string Name, string Firstname, string Pseudo, string Email, string PicturePath, string Bio, string? Id = null, string? Password = null, string? token = null);
-}
---
+++ Quiz.Dtos/UserDto.cs
@@ -0,0 +1,4 @@
+﻿namespace Quiz.Dtos
+{
+    public record UserDto(string Name, string Firstname, string Pseudo, string Email, string PicturePath, string Bio, string? Id = null, string? Password = null, string? Birthday = null, string? token = null);
+}
