1e492b1f1fa6cd551ae397bb42441fb2563e8d3c
Cyril Auquier <cyrilauquier@hotmail.fr>
2024-08-26 12:13:02 +0200

    send register data to api

--- Quiz.ForNative.Repository/AuthApiRepository.cs
+++ Quiz.ForNative.Repository/AuthApiRepository.cs
@@ -1,6 +1,7 @@
 ﻿using Newtonsoft.Json;
-using Quiz.Dtos.Api;
+using Quiz.Dtos;
 using Quiz.ForNative.Repository.Interfaces;
+using System.Net.Http.Headers;
 
 namespace Quiz.ForNative.Repository
 {
@@ -13,10 +14,26 @@ namespace Quiz.ForNative.Repository
             _httpClient = httpClient;
         }
 
-        public async Task<UserDto> RegisterUser(UserDto user)
+        public async Task<UserDto> RegisterUser(UserDto user, FileStream fileStream)
         {
-            HttpResponseMessage response = (await _httpClient.PostAsJsonAsync("/auth/register", user));
-            return JsonConvert.DeserializeObject<UserDto>(await response.Content.ReadAsStringAsync())!;
+            // TODO : Handle error 400 (maybe with a middleware)
+            // TODO : Create DTO to get api errors
+            MultipartFormDataContent multipartFormContent = new MultipartFormDataContent
+            {
+                { new StringContent(user.Bio), nameof(UserDto.Bio).ToLower() },
+                { new StringContent(user.Birthday), "birthDate" },
+                { new StringContent(user.Firstname), nameof(UserDto.Firstname).ToLower() },
+                { new StringContent(user.Name), nameof(UserDto.Name).ToLower() },
+                { new StringContent(user.Email), nameof(UserDto.Email).ToLower() },
+                { new StringContent(user.Pseudo), nameof(UserDto.Pseudo).ToLower() },
+                { new StringContent(user.Password), nameof(UserDto.Password).ToLower() }
+            };
+            StreamContent fileStreamContent = new StreamContent(fileStream);
+            fileStreamContent.Headers.ContentType = new MediaTypeHeaderValue("image/png");
+            multipartFormContent.Add(fileStreamContent, name: "file", fileName: $"{user.Pseudo}.png");
+            HttpResponseMessage response = (await _httpClient.PostAsync<UserDto>("/auth", multipartFormContent));
+            string responseContent = await response.Content.ReadAsStringAsync();
+            return JsonConvert.DeserializeObject<UserDto>(responseContent)!;
         }
     }
 }
