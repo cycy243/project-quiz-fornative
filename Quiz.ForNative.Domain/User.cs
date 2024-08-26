ff6d28ed6df1b4cdbc9fbbc846586cebf917ccb3
Cyril Auquier <cyrilauquier@hotmail.fr>
2024-08-26 12:18:28 +0200

    refactor and create separate project for app classes

---
+++ Quiz.ForNative.Domain/User.cs
@@ -0,0 +1,4 @@
+﻿namespace Quiz.ForNative.Domain
+{
+    public record User(string Name, string Firstname, string Pseudo, string Email, string PicturePath, string Bio, string? Id = null, string? Password = null);
+}
