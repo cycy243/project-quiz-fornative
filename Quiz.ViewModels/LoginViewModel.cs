51b9c028b7ff5f1147057c7be10216cac0d82c5f
Cyril Auquier <cyrilauquier@hotmail.fr>
2024-09-02 13:07:00 +0200

    US-2.5: - add viewmodels

---
+++ Quiz.ViewModels/LoginViewModel.cs
@@ -0,0 +1,38 @@
+﻿using Quiz.ForNative.Domain;
+using Quiz.ForNative.Services.Exceptions;
+using Quiz.ForNative.Services.Interface.Auth;
+using Quiz.ViewModels.Exceptions;
+using Quiz.ViewModels.Interface;
+
+namespace Quiz.ViewModels
+{
+    public class LoginViewModel : IConnectViewModel
+    {
+        private IConnectService<User> _connectService;
+
+        public string Login { get; set; }
+        public string Password { get; set; }
+
+        public LoginViewModel(IConnectService<User> connectService)
+        {
+            _connectService = connectService;
+        }
+
+        public async Task<bool> LogUserIn()
+        {
+            try
+            {
+                User? result = await _connectService.ConnectWithCredentialsAsync(Login, Password);
+                return result != null;
+            }
+            catch (ServiceException se)
+            {
+                throw new ViewModelException(se.Message);
+            }
+            catch (Exception ex)
+            {
+                throw new ViewModelException($"Unexpected error");
+            }
+        }
+    }
+}
